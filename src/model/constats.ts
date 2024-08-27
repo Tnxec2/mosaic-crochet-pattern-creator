f76ede5d3103be5a433f34b98bde023d31f4ef1e
tnxec2 <tnxec2@gmail.com>
2024-08-27 14:16:13 +0200

    v. 1.1.4 build

--- src/model/constats.ts
+++ src/model/constats.ts
@@ -2,6 +2,6 @@ export const BACKGROUND_COLOR = '#ffffff'
 export const DEFAULT_COLOR = '#ffffff'
 export const DEFAULT_COLOR_2 = '#FF0000'
 export const KEY_STORAGE = 'com.kontranik.mosaiccrochetpattern.state'
-export const VERSION = '1.1.3'
+export const VERSION = '1.1.4'
 export const UNKNOWN_NAME = 'unnamed pattern'
 export const DEFAULT_FONT_SIZE = 10
