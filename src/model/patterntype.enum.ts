793c4c9dd24f5e860cca783f07793757781a593c
tnxec2 <tnxec2@gmail.com>
2025-02-05 11:22:50 +0100

    actionToCellType

--- src/model/patterntype.enum.ts
+++ src/model/patterntype.enum.ts
@@ -1,3 +1,5 @@
+import { ACTION_TYPES } from "./actiontype.enum";
+
 export enum CELL_TYPE {
     EMPTY = '',
     X = 'x',
@@ -7,4 +9,27 @@ export enum CELL_TYPE {
     XR = 'xr',
     LX = 'lx',
     LXR = 'lxr'
+}
+
+export function actionToCellType(action: ACTION_TYPES, type: CELL_TYPE): CELL_TYPE {
+    switch (action) {
+        case ACTION_TYPES.NONE:
+            return CELL_TYPE.EMPTY
+        case ACTION_TYPES.X:
+            return CELL_TYPE.X
+        case ACTION_TYPES.LR:
+            return CELL_TYPE.LR
+        case ACTION_TYPES.L:
+            return CELL_TYPE.L
+        case ACTION_TYPES.R:
+            return CELL_TYPE.R
+        case ACTION_TYPES.LX:
+            return CELL_TYPE.LX
+        case ACTION_TYPES.XR:
+            return CELL_TYPE.XR
+        case ACTION_TYPES.LXR:
+            return CELL_TYPE.LXR
+        default:
+            return type;
+    }
 }
