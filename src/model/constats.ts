9c882f8078b8fe0f7cfe2c02bde6bc231621d523
tnxec2 <tnxec2@gmail.com>
2025-07-03 09:57:22 +0200

    build v. 1.3.6

--- src/model/constats.ts
+++ src/model/constats.ts
@@ -1,6 +1,6 @@
 import { TVIEWBOX_SIZE } from "./patterntype.enum"
 
-export const VERSION = '1.3.5'
+export const VERSION = '1.3.6'
 
 export const BACKGROUND_COLOR = '#ffffff'
 export const DEFAULT_COLOR = '#ffffff'
