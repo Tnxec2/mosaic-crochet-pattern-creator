c525a4720b40401852df04d3b934f491851905dd
tnxec2 <tnxec2@gmail.com>
2025-07-03 09:56:19 +0200

    clean

--- src/components/panel/panel.tsx
+++ src/components/panel/panel.tsx
@@ -2,7 +2,6 @@ import { ColorsComponent } from './colors'
 import { PatternSizeComponent } from './patternsize'
 import { ActionsComponent } from './actions'
 import { PropertiesComponent } from './properties'
-import { MinimapPanelComponent } from './minimap'
 import { PatternMinimapComponent } from '../pattern/windowed/pattern.minimap'
 
 export const PanelComponent = () => {
